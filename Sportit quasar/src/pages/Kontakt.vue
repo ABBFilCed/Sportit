554f67b0c9e51751053ce82cca63193c90779708
ABBFilCed <43879979+ABBFilCed@users.noreply.github.com>
2020-09-23 16:05:12 +0200

    started_cards

--- Sportit quasar/src/pages/Kontakt.vue
+++ Sportit quasar/src/pages/Kontakt.vue
@@ -6,7 +6,44 @@
         :height="150"
       />
     </q-card>
-    <div class="text-center q-pa-xl" style="height:700px">Jag heter filip och har ett telefon nummer haj hej<br>Jegh heter jenny<br>Filip är mitt namn med<br><br>Vi använda social media ogha bogha</div>
+    <div v-for="member in members" :key=member.id class="col-4" style="width:33%">
+      <q-card class="my-card" flat bordered style="">
+        <q-img
+          :src=member.image
+        />
+
+        <q-card-section>
+          <div class="text-h5 q-mt-sm q-mb-xs">{{member.member}}</div>
+          <div class="text-caption text-grey">
+            {{member.job}}
+          </div>
+        </q-card-section>
+
+        <q-card-actions>
+
+          <q-space />
+
+          <q-btn
+            color="grey"
+            rounded
+            flat
+            dense
+            style="width:100%"
+            :icon=" member.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+            @click="member.expanded = !member.expanded"
+          />
+        </q-card-actions>
+
+        <q-slide-transition>
+          <div v-show="member.expanded">
+            <q-separator />
+            <q-card-section class="text-subitle2">
+              {{member.description}}
+            </q-card-section>
+          </div>
+        </q-slide-transition>
+      </q-card>
+    </div>
     <bottom-information></bottom-information>
   </q-page>
 </template>
@@ -16,6 +53,16 @@ import BottomInformation from '../components/BottomInformation.vue'
 export default {
   components: {
     BottomInformation
+  },
+  data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      members: [
+        { id: 1, member: 'Filip Segerberg', job: 'VD', description: 'jabags', image: '../images/lillafille.png', expanded: false },
+        { id: 2, member: 'Filip Cederblad', job: 'Webbdesigner', description: 'jabags', image: '../images/lillafille.png', expanded: false },
+        { id: 3, member: 'Jenny Melkersson', job: 'Ekonom', description: 'jabags', image: '../images/lillafille.png', expanded: false }
+      ]
+    }
   }
 }// importerat bottominformationcomponenten
 </script>
