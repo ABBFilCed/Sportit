b7ff3a6662614132115b8012168c3f73640ed7ad
ABBFilCed <43879979+ABBFilCed@users.noreply.github.com>
2020-09-29 10:27:09 +0200

    images_in_cards

--- Sportit quasar/src/pages/Kontakt.vue
+++ Sportit quasar/src/pages/Kontakt.vue
@@ -6,43 +6,48 @@
         :height="150"
       />
     </q-card>
-    <div v-for="member in members" :key=member.id class="col-4" style="width:33%">
-      <q-card class="my-card" flat bordered style="">
-        <q-img
-          :src=member.image
-        />
-
-        <q-card-section>
-          <div class="text-h5 q-mt-sm q-mb-xs">{{member.member}}</div>
-          <div class="text-caption text-grey">
-            {{member.job}}
-          </div>
-        </q-card-section>
-
-        <q-card-actions>
-
-          <q-space />
-
-          <q-btn
-            color="grey"
-            rounded
-            flat
-            dense
-            style="width:100%"
-            :icon=" member.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-            @click="member.expanded = !member.expanded"
+    <div class="row q-ma-xl q-gutter-xl">
+      <div v-for="member in members" :key=member.id class="col">
+        <q-card class="my-card" flat bordered style="">
+          <q-img
+            style="height:300px"
+            :src='member.image'
           />
-        </q-card-actions>
 
-        <q-slide-transition>
-          <div v-show="member.expanded">
-            <q-separator />
-            <q-card-section class="text-subitle2">
-              {{member.description}}
-            </q-card-section>
-          </div>
-        </q-slide-transition>
-      </q-card>
+          <q-card-section>
+            <div class="text-h5 q-mt-sm q-mb-xs">{{member.member}}</div>
+            <div class="text-caption text-grey">
+              {{member.job}}
+            </div>
+          </q-card-section>
+
+          <q-card-actions>
+
+            <q-space />
+
+            <q-btn
+              color="grey"
+              rounded
+              flat
+              dense
+              style="width:100%"
+              :icon=" member.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+              @click="member.expanded = !member.expanded"
+            />
+          </q-card-actions>
+
+          <q-slide-transition>
+            <div v-show="member.expanded">
+              <q-separator />
+              <q-card-section class="text-subitle2">
+                Tel.nummer: {{member.number}}<br><br>
+                Email: {{member.email}}<br><br>
+                {{member.description}}
+              </q-card-section>
+            </div>
+          </q-slide-transition>
+        </q-card>
+      </div>
     </div>
     <bottom-information></bottom-information>
   </q-page>
@@ -58,9 +63,9 @@ export default {
     return {
       lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
       members: [
-        { id: 1, member: 'Filip Segerberg', job: 'VD', description: 'jabags', image: '../images/lillafille.png', expanded: false },
-        { id: 2, member: 'Filip Cederblad', job: 'Webbdesigner', description: 'jabags', image: '../images/lillafille.png', expanded: false },
-        { id: 3, member: 'Jenny Melkersson', job: 'Ekonom', description: 'jabags', image: '../images/lillafille.png', expanded: false }
+        { id: 1, member: 'Filip Segerberg', job: 'VD', description: 'jabags', number: '123', email: 'email', image: require('../images/lillafille.png'), expanded: false },
+        { id: 2, member: 'Filip Cederblad', job: 'Webbdesigner', description: 'jabags', number: '123', email: 'email', image: require('../images/FilleC.jpg'), expanded: false },
+        { id: 3, member: 'Jenny Melkersson', job: 'Ekonom', description: 'jabags', number: '123', email: 'email', image: require('../images/jenny.png'), expanded: false }
       ]
     }
   }
