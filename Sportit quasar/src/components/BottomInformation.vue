8f692e0300d006d53ae4322bfba8c85495174f46
Shrimpen <43981768+Shrimpen@users.noreply.github.com>
2020-09-29 10:28:01 +0200

    Login
    Stylen för loggin är klar

--- Sportit quasar/src/components/BottomInformation.vue
+++ Sportit quasar/src/components/BottomInformation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="background-color:dimgray; height: 130px;" class="row q-mt-lg">
+  <div style="background-color:dimgray; height: 130px; width: 100%;" class="row q-mt-lg">
     <div class="col text-center text-white q-pt-md" style="font-size:3em">
       <q-btn flat round size="xl" type="a" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" icon="facebook"/>
       <q-btn flat round size="xl" type="a" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" icon="ion-logo-twitter"/>
