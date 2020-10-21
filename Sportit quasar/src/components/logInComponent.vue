1b829f1fb9fb4547cfb74ec1116f4a687ee963de
ABBFilCed <43879979+ABBFilCed@users.noreply.github.com>
2020-10-21 15:46:34 +0200

    rules

--- Sportit quasar/src/components/logInComponent.vue
+++ Sportit quasar/src/components/logInComponent.vue
@@ -2,34 +2,64 @@
   <div
   class="row justify-center items-center"
   >
-    <div class="column">
+    <q-form class="column">
       <div class="row">
         <q-card square class="" style="width:450px;height:640px;">
           <q-card-section class="text-center" style="background-color: dodgerblue; height:150px">
             <q-img id="logo" src="../images/sportit-logga-jenny-liten.png" style="width:120px"></q-img>
+            <q-btn icon="close" flat round dense v-close-popup class="absolute-top-right q-ma-sm" type="reset" />
           </q-card-section>
           <q-card-section>
             <q-form class="q-gutter-md">
-              <q-input square clearable v-model="email" type="email" label="Email" class="q-mt-md">
+              <q-input
+               square
+               clearable
+               v-model="email"
+               type="email"
+               label="Email"
+               class="q-mt-md"
+               lazy-rules
+                :rules="[ val => val && val.length > 0 || 'Please type something']"
+               >
                 <template v-slot:prepend>
                   <q-icon name="email" />
                 </template>
               </q-input>
-              <q-input square clearable v-model="password" type="password" label="password">
+              <q-input
+              square
+              clearable
+              v-model="password"
+              type="password"
+              label="password"
+              lazy-rules
+              :rules="[ val => val && val.length > 7 || 'Lösenordet måste vara minst 8 tecken']"
+              >
                 <template v-slot:prepend>
                   <q-icon name="lock" />
                 </template>
               </q-input>
             </q-form>
             <q-card-actions class="q-mt-lg">
-              <q-btn unelevated size="lg" class="full-width text-white q-mt-lg" style="background-color:#404040;" label="Logga in" />
-              <q-btn size="lg" class="full-width q-mt-lg" outline color="#404040" label="Registrera dig" @click="register = true" />
+              <q-btn
+              unelevated
+              size="lg"
+              class="full-width text-white q-mt-lg"
+              style="background-color:#404040;"
+              label="Logga in"
+              type="submit" />
+              <q-btn
+              size="lg"
+              class="full-width q-mt-lg"
+              outline
+              color="#404040"
+              label="Registrera dig"
+              @click="register = true" />
             </q-card-actions>
           </q-card-section>
           <q-card-section class="text-center q-pa-sm">
             <p class="text-grey-6">Glömt lösenordet?</p>
           </q-card-section>
-          <q-card-section>
+          <!-- <q-card-section>
             <div class="text-center q-pa-md q-gutter-md" style="margin-top:-50px">
               <q-btn round color="indigo-7">
                 <q-icon name="fab fa-facebook-f" size="1.2rem" />
@@ -41,10 +71,10 @@
                 <q-icon name="fab fa-twitter" size="1.2rem" />
               </q-btn>
             </div>
-          </q-card-section>
+          </q-card-section> -->
         </q-card>
       </div>
-    </div>
+    </q-form>
     <q-dialog v-model="register" seamless>
       <registerComponent/>
     </q-dialog>
--- Sportit quasar/src/components/registerComponent.vue
+++ Sportit quasar/src/components/registerComponent.vue
@@ -2,38 +2,72 @@
   <div
   class="row justify-center items-center"
   >
-    <div class="column">
+    <q-form class="column">
       <div class="row">
         <q-card square class="" style="width:450px;height:640px;">
           <q-card-section class="text-center" style="background-color: dodgerblue; height:150px; color:white;">
             <h3 style="">Registrera dig</h3>
+            <q-btn type="reset" icon="arrow_back" flat round dense v-close-popup color="black" class="absolute-top-left q-ma-sm" />
           </q-card-section>
           <q-card-section>
             <q-form class="q-gutter-md">
-              <q-input square clearable v-model="email" type="email" label="Email" class="q-mt-md">
+              <q-input
+              square
+              clearable
+              v-model="email"
+              type="email"
+              label="Email"
+              class="q-mt-md"
+              lazy-rules
+              :rules="[ val => val && val.length > 0 || 'Skriv något']"
+               >
                 <template v-slot:prepend>
                   <q-icon name="email" />
                 </template>
               </q-input>
-              <q-input square clearable v-model="username" type="username" label="Username" class="q-mt-md">
-                <template v-slot:prepend>
-                  <q-icon name="person" />
-                </template>
-              </q-input>
-              <q-input square clearable v-model="password" type="password" label="password">
+              <q-input
+              square
+              clearable
+              v-model="password"
+              type="password"
+              label="password"
+              class="q-mt-md"
+              lazy-rules
+              :rules="passwordRules"
+              >
                 <template v-slot:prepend>
                   <q-icon name="lock" />
                 </template>
               </q-input>
+              <q-input
+              square
+              clearable
+              v-model="repassword"
+              type="password"
+              label="confirm password"
+              lazy-rules
+              :rules="confirmPasswordRules"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="check" />
+                </template>
+              </q-input>
             </q-form>
             <div class="q-gutter-sm">
-              <q-checkbox v-model="agreement" label="Jag godkänner Avtal och villkor" class="q-mt-md"/>
+              <q-toggle v-model="agreement" label="Jag godkänner Avtal och villkor" class="q-mt-md"/>
             </div>
             <q-card-actions class="q-mt-lg">
-              <q-btn unelevated size="lg" class="full-width text-white q-mt-lg" style="background-color:#404040;" label="Kom igång!" />
+              <q-btn
+              unelevated
+              size="lg"
+              class="full-width text-white q-mt-lg"
+              style="background-color:#404040;"
+              label="Kom igång!"
+              type="submit"
+              />
             </q-card-actions>
           </q-card-section>
-          <q-card-section>
+          <!-- <q-card-section>
             <div class="text-center q-pa-md q-gutter-md" style="margin-top:-50px">
               <q-btn round color="indigo-7">
                 <q-icon name="fab fa-facebook-f" size="1.2rem" />
@@ -45,10 +79,10 @@
                 <q-icon name="fab fa-twitter" size="1.2rem" />
               </q-btn>
             </div>
-          </q-card-section>
+          </q-card-section> -->
         </q-card>
       </div>
-    </div>
+    </q-form>
   </div>
 </template>
 
@@ -58,9 +92,30 @@ export default {
   data () {
     return {
       email: '',
-      username: '',
+      repassword: '',
       password: '',
-      agreement: false
+      agreement: false,
+      passwordRules: [
+        (value) => !!value || 'Please type password.',
+        (value) => (value && value.length >= 6) || 'minimum 6 characters'
+      ],
+      confirmPasswordRules: [
+        (value) => !!value || 'type confirm password',
+        (value) =>
+          value === this.password || 'The password confirmation does not match.'
+      ]
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.agreement !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'Acceptera vilkor för att skapa konto'
+        })
+      }
     }
   }
 }
