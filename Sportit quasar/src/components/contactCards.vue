ba0afe632b9f5cbd315ba1c19a77e27a02a25399
ABBFilCed <43879979+ABBFilCed@users.noreply.github.com>
2020-11-17 15:17:12 +0100

    fade in/ images in homepage
    fade in text. Personal images added to homepage. Parralax search bar and images

--- Sportit quasar/src/components/contactCards.vue
+++ Sportit quasar/src/components/contactCards.vue
@@ -1,47 +1,40 @@
-<template>
-  <div class="row q-ma-xl q-gutter-xl">
-    <div v-for="member in members" :key=member.id class="col">
-      <q-card class="my-card" flat bordered style="">
-        <q-img
-          style="max-height:300px"
-          :src='member.image'
-        />
+<template >
+  <q-parallax class="" speed="0.5" :height="1200">
+    <template v-slot:media>
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+    </template>
 
-        <q-card-section>
-          <div class="text-h5 q-mt-sm q-mb-xs">{{member.member}}</div>
-          <div class="text-caption text-grey">
-            {{member.job}}
+    <template v-slot:content="scope" class="col">
+      <div
+        class="absolute column items-center row"
+        :style="{
+          opacity: 0.80 + (1 - scope.percentScrolled) *0.55,
+          bottom: (scope.percentScrolled * 60) + '%',
+          left: 0,
+          right: 0
+        }"
+      >
+      <div style="color:white" class="container row">
+        <h1 class="text-center">Vi på SportIT</h1>
+      </div>
+        <div class="row q-ma-xl q-gutter-xl justify-end" style="max-width:80%">
+          <div v-for="member in members" :key=member.id class="col self-end" style="max-height:600px" >
+            <q-img
+              style="max-height:300px; border-radius: 300px; max-width:300px;"
+              :src='member.image'
+            />
+            <div style="color:white;">
+              <h3>{{member.member}}</h3>
+              <p>{{member.job}}</p>
+              <p>Telefon: {{member.number}}</p>
+              <p>E-mail: {{member.email}}</p>
+              <p>{{member.description}}</p>
+            </div>
           </div>
-        </q-card-section>
-
-        <q-card-actions>
-
-          <q-space />
-
-          <q-btn
-            color="grey"
-            rounded
-            flat
-            dense
-            style="width:100%"
-            :icon=" member.expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-            @click="member.expanded = !member.expanded"
-          />
-        </q-card-actions>
-
-        <q-slide-transition>
-          <div v-show="member.expanded">
-            <q-separator />
-            <q-card-section class="text-subitle2">
-              Tel.nummer: {{member.number}}<br><br>
-              Email: {{member.email}}<br><br>
-              {{member.description}}
-            </q-card-section>
-          </div>
-        </q-slide-transition>
-      </q-card>
-    </div>
-  </div>
+        </div>
+      </div>
+    </template>
+  </q-parallax>
 </template>
 
 <script>
@@ -49,15 +42,55 @@ export default {
   data () {
     return {
       members: [
-        { id: 1, member: 'Filip Segerberg', job: 'VD', description: 'jabags', number: '123', email: 'email', image: require('../images/lillafille.png'), expanded: false },
-        { id: 2, member: 'Filip Cederblad', job: 'Webbdesigner', description: 'jabags', number: '123', email: 'email', image: require('../images/FilleC.jpg'), expanded: false },
-        { id: 3, member: 'Jenny Melkersson', job: 'Ekonom', description: 'jabags', number: '123', email: 'email', image: require('../images/jenny.png'), expanded: false }
-      ]
+        { id: 1, member: 'Filip Segerberg', job: 'VD', description: 'jabags', number: '123 567 67 67', email: 'Filip.Segerberg@SportIT.se', image: require('../images/lillafille.png'), expanded: false },
+        { id: 2, member: 'Filip Cederblad', job: 'Webbdesigner', description: 'jabags', number: '123 873 60 01', email: 'Filip.Cederblad@SportIT.se', image: require('../images/FilleC.jpg'), expanded: false },
+        { id: 3, member: 'Jenny Melkersson', job: 'Ekonom', description: 'jabags', number: '123 679 89 08', email: 'Jenny.Melkersson@SportIT.se', image: require('../images/jenny.png'), expanded: false }
+      ],
+      fadeInElements: [],
+      fadeOutElements: []
     }
+  },
+  methods: {
+    isElemVisible (el) {
+      var rect = el.getBoundingClientRect()
+      var elemTop = rect.top + 200 // 200 = buffer
+      var elemBottom = rect.bottom - 200
+      return elemTop < window.innerHeight && elemBottom >= 0
+    },
+    handleScroll (evt) {
+      for (var i = 0; i < this.fadeInElements.length; i++) {
+        var elem = this.fadeInElements[i]
+        if (this.isElemVisible(elem)) {
+          elem.style.opacity = '1'
+          elem.style.transform = 'scale(1)'
+        }
+      }
+      for (var b = 0; b < this.fadeOutElements.length; b++) {
+        var element = this.fadeOutElements[b]
+        if (!(this.isElemVisible(element))) {
+          element.style.opacity = '0'
+          element.style.transform = 'scale(1)'
+        }
+      }
+    }
+  },
+  mounted () {
+    this.fadeInElements = Array.from(document.getElementsByClassName('fade-in'))
+    this.fadeOutElements = Array.from(document.getElementsByClassName('fade-in'))
+    document.addEventListener('scroll', this.handleScroll)
+    this.handleScroll()
+  },
+  unmounted () {
+    document.removeEventListener('scroll', this.handleScroll)
   }
 }
 </script>
 
 <style>
-
+.fade-in{
+  opacity:0;
+  transition: 4s;
+  transform: scale(0.8);
+  transform-origin: left;
+}
 </style>
