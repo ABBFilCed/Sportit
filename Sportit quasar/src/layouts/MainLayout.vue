2c589aaa7f4443e379e1281168c9d3bd05cefd1e
abbjenmel <43954737+abbjenmel@users.noreply.github.com>
2021-02-23 08:29:19 +0100

    he

--- Sportit quasar/src/layouts/MainLayout.vue
+++ Sportit quasar/src/layouts/MainLayout.vue
@@ -1,21 +1,19 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar id="toolbar" class="row" style="padding:10px; background-color:dodgerblue;">
+      <q-toolbar id="toolbar" class="row justify-center" style="padding:8px; background-color:dodgerblue;">
         <!-- <q-scroll-observer @scroll="onScroll"/> -->
-        <div class="row justify-end">
-          <q-toolbar-title  class="col">
+          <!-- <q-toolbar-title  class="col"> -->
             <!-- <router-link to="/" exact style="background-color:dodgerblue; margin-left:15px; margin-top:15px; margin-bottom:15px;"><q-img id="logo" src="../images/logo.jpg" width="50px" height="50px"></q-img></router-link> -->
-          </q-toolbar-title><!-- länk till main sidan med logga -->
+          <!-- </q-toolbar-title> -->
 
-          <div class=" justify-end" style="list-style-type:none">
+          <div class="" style="list-style-type:none">
             <li class="col-xs-2 col"><router-link to="/" exact style="background-color:dodgerblue; margin-top:15px; margin-bottom:15px;"><q-img id="logo" src="../images/logo.jpg" width="50px" height="50px"></q-img></router-link></li>
             <li class="col-xs-2 col"><router-link to="/kontakt" exact>KONTAKT</router-link></li>
             <li class="col-xs-2 col"><router-link to="/klubben" exact>KLUBBEN</router-link></li>
             <li class="col-xs-2 col"><router-link to="/anlaggningar" exact> ANLÄGGNINGAR </router-link></li>
-            <q-btn flat no-caps style="font-size: 15px; font-weight:bold; margin:0px 5px;  margin-bottom: 5px;" :ripple="false" class="col-xs-2 col" label="LOGGA IN" color="" @click="login = true" />
+            <li class="col-xs-2 col"><q-btn flat no-caps style="font-size: 15px; font-weight:bold; margin-bottom: 2px; " :ripple="false" label="LOGGA IN" color="" @click="login = true" /></li>
           </div><!-- länkar till olika sidor -->
-        </div>
       </q-toolbar>
     </q-header>
     <div>
