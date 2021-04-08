94790e5f6b4eac375ad0f1d73c958449e106448b
abbjenmel <43954737+abbjenmel@users.noreply.github.com>
2021-04-08 11:29:15 +0200

    kontakt

--- Sportit quasar/src/pages/Kontakt.vue
+++ Sportit quasar/src/pages/Kontakt.vue
@@ -6,19 +6,19 @@
         :height="150"
       />
     </q-card>
-    <h1 class="text-center">Kontakta oss</h1>
+    <h1 class="row justify-center Teko">Kontakt</h1>
     <div style="background-color:dodgerblue; color:white; width:100%;">
-      <div class="q-ma-xl q-pa-xl text-center">
-        <p>{{info.info1}}</p><br><br>
-        <p>Email: {{info.email}}</p><br>
-        <p>Address: {{info.address}}</p>
+      <div class="row justify-center">
+        <h5 class="col-md-6 col-xs-11 text-center">{{info.info1}}</h5><br>
       </div>
+      <h6 class="row justify-center">Email: {{info.email}}</h6>
+      <h6 class="row q-pb-lg justify-center">Address: {{info.address}}</h6>
     </div>
-    <h1 class="text-center">Vi på Sportit</h1>
+    <h1 class="text-center">Vi på SportIT</h1>
     <contact-cards/>
     <div style="background-color:dodgerblue; color:white; width:100%;">
-      <div class="q-mx-xl q-pa-xl text-center">
-        <p>{{info.info2}}</p><br><br>
+      <div class="row q-py-lg justify-center">
+        <p class="col-md-6 col-xs-11 text-center">{{info.info2}}</p><br><br>
       </div>
     </div>
     <bottom-information></bottom-information>
@@ -36,8 +36,8 @@ export default {
   data () {
     return {
       info: {
-        info1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        email: 'info@sportit.se',
+        info1: 'Vi på SportIT svarar gärna på era frågor och funderingar, det går bra att höra av sig på antingen mail eller telefon.',
+        email: 'info@sport-it.se',
         info2: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         address: 'Robotvägen 4 Västerås'
       }
@@ -47,5 +47,8 @@ export default {
 </script>
 
 <style>
+.Teko {
+  font-family: 'Teko-Light';
+}
 
 </style>
